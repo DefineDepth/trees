65621a4812ce4989fefa820bc4aea466e3bd6f26
DefineDepth <masterov.mixail@gmail.com>
2022-02-18 19:09:22 +0300

    last

--- resources/assets/scripts/components/FrontPageText.js
+++ resources/assets/scripts/components/FrontPageText.js
@@ -1,7 +1,4 @@
-// import APP from '../util/global';
-// import ScrollMagic from 'scrollmagic'
 import gsap from '../vendors/gsap.min.js'
-// import SplitText from '../vendors/SplitText.min.js'
 
 export default class FrontPageText {
   constructor() {
